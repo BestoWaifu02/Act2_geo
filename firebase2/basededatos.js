3c2bd09d344b06f42849bcfc7aa0626e18d0934c
BestoWaifu02 <BestoWaifu02@gmail.com>
2021-05-03 15:00:03 -0500

    firebase practicas

---
+++ firebase2/basededatos.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBmZKLaf0pQ2pUNWvjtXU5DKEYc5iLDrMY",
+  authDomain: "practica1-e1b15.firebaseapp.com",
+  projectId: "practica1-e1b15",
+  storageBucket: "practica1-e1b15.appspot.com",
+  messagingSenderId: "61070507903",
+  appId: "1:61070507903:web:465cc3c7e6a6393745244a",
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
